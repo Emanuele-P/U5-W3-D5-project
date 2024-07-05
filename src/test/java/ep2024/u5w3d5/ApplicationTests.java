a9131c90e20583e71b8cd11cabf12a9aaf50c0a5
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-07-05 12:07:22 +0200

    initialize project

---
+++ src/test/java/ep2024/u5w3d5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ep2024.u5w3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
